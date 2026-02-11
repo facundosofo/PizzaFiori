0b4fd2c217c6e5b30327c4ace138c01988a59c31
facundosofo <facundosofo@gmail.com>
2026-02-11 19:10:35 -0300

    Cambio mensaje en el carrito

--- frontend/src/components/CartDrawer.tsx
+++ frontend/src/components/CartDrawer.tsx
@@ -56,7 +56,7 @@ export const CartDrawer: React.FC<CartDrawerProps> = ({
               <Icons.ShoppingCartIcon size={48} color="rgba(255, 255, 255, 0.3)" />
             </div>
             <p className="cart-empty-text">
-              Agrega productos o ofertas para comenzar
+              Agrega productos u ofertas para comenzar
             </p>
           </div>
         ) : (
